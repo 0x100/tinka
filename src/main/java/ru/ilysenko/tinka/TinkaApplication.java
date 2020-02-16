de869e660ee81d1e3925c13d251c3a152bf8c6a4
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-02-17 00:37:01 +0300

    Spring Boot app

---
+++ src/main/java/ru/ilysenko/tinka/TinkaApplication.java
@@ -0,0 +1,13 @@
+package ru.ilysenko.tinka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ilysenko/tinka/TinkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ilysenko.tinka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
