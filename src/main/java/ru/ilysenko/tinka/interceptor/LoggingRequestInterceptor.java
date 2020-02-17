a6d9af7f2a54da2b3b4c99ae2d878d7f60044370
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-02-18 01:57:00 +0300

    Update output of the request logging interceptor

--- src/main/java/ru/ilysenko/tinka/interceptor/LoggingRequestInterceptor.java
+++ src/main/java/ru/ilysenko/tinka/interceptor/LoggingRequestInterceptor.java
@@ -1,17 +1,26 @@
 package ru.ilysenko.tinka.interceptor;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
 import org.springframework.http.HttpRequest;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Slf4j
 public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
+    private ObjectMapper objectMapper;
+
+    public LoggingRequestInterceptor() {
+        objectMapper = new ObjectMapper();
+    }
 
     @Override
     public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
@@ -23,19 +32,42 @@ public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
         return response;
     }
 
+    @SneakyThrows
     private void traceRequest(HttpRequest request, byte[] body) {
+        String content = new String(body, StandardCharsets.UTF_8);
+
         log.debug("===========================request begin================================================");
         log.debug("URI         : {}", request.getURI());
         log.debug("Method      : {}", request.getMethod());
-        log.debug("Headers     : {}", request.getHeaders() );
-        log.debug("Request body: {}", new String(body, StandardCharsets.UTF_8));
+        log.debug("Headers     : {}", request.getHeaders());
+        log.debug("Request body: \n{}", beautifyBody(content));
     }
 
     private void traceResponse(ClientHttpResponse response) throws IOException {
+        String content = IOUtils.toString(response.getBody(), StandardCharsets.UTF_8.name());
+
         log.debug("============================response begin==========================================");
         log.debug("Status code  : {}", response.getStatusCode());
         log.debug("Status text  : {}", response.getStatusText());
         log.debug("Headers      : {}", response.getHeaders());
-        log.debug("Response body: {}", IOUtils.toString(response.getBody(), StandardCharsets.UTF_8.name()));
+        log.debug("Response body: \n{}", beautifyBody(content));
+    }
+
+    private String beautifyBody(String content) throws JsonProcessingException {
+        if(StringUtils.hasText(content) && isValidJson(content)) {
+            Object json = objectMapper.readValue(content, Object.class);
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
+        } else {
+            return content;
+        }
+    }
+
+    private boolean isValidJson(String json) {
+        try {
+            objectMapper.readTree(json);
+        } catch (JsonProcessingException e) {
+            return false;
+        }
+        return true;
     }
 }
