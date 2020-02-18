07dccda8effe4ca8e6abc95eab8c227c1a2a4fb0
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-02-19 00:14:49 +0300

    Fix tests

--- src/main/java/ru/ilysenko/tinka/example/MarketApiExample.java
+++ src/main/java/ru/ilysenko/tinka/example/MarketApiExample.java
@@ -3,6 +3,7 @@ package ru.ilysenko.tinka.example;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.threeten.bp.LocalTime;
@@ -18,6 +19,7 @@ import java.util.Optional;
 
 @Slf4j
 @Component
+@Profile("!test")
 @RequiredArgsConstructor
 public class MarketApiExample {
 
