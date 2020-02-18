745094a79674deedcf5ce1a2a7ced027500067d5
Ilya Lysenko <lysenko.ilya@gmail.com>
2020-02-18 05:44:56 +0300

    Refactoring

--- src/main/java/ru/ilysenko/tinka/config/RestConfiguration.java
+++ src/main/java/ru/ilysenko/tinka/config/RestConfiguration.java
@@ -1,13 +1,11 @@
 package ru.ilysenko.tinka.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.threetenbp.ThreeTenModule;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import org.threeten.bp.Instant;
@@ -27,17 +25,23 @@ public class RestConfiguration {
                 .interceptors(new RequestEncodingInterceptor(), new LoggingRequestInterceptor())
                 .defaultMessageConverters()
                 .build();
+        addDateDeserializers(restTemplate);
 
-        for (HttpMessageConverter converter : restTemplate.getMessageConverters()) {
-            if (converter instanceof AbstractJackson2HttpMessageConverter) {
-                ObjectMapper mapper = ((AbstractJackson2HttpMessageConverter) converter).getObjectMapper();
-                ThreeTenModule module = new ThreeTenModule();
-                module.addDeserializer(Instant.class, CustomInstantDeserializer.INSTANT);
-                module.addDeserializer(OffsetDateTime.class, CustomInstantDeserializer.OFFSET_DATE_TIME);
-                module.addDeserializer(ZonedDateTime.class, CustomInstantDeserializer.ZONED_DATE_TIME);
-                mapper.registerModule(module);
-            }
-        }
         return restTemplate;
+    }
+
+    private void addDateDeserializers(RestTemplate restTemplate) {
+        restTemplate.getMessageConverters().stream()
+                .filter(c -> c instanceof AbstractJackson2HttpMessageConverter)
+                .findFirst()
+                .map(c -> (AbstractJackson2HttpMessageConverter) c)
+                .map(AbstractJackson2HttpMessageConverter::getObjectMapper)
+                .ifPresent(m -> {
+                    ThreeTenModule module = new ThreeTenModule();
+                    module.addDeserializer(Instant.class, CustomInstantDeserializer.INSTANT);
+                    module.addDeserializer(OffsetDateTime.class, CustomInstantDeserializer.OFFSET_DATE_TIME);
+                    module.addDeserializer(ZonedDateTime.class, CustomInstantDeserializer.ZONED_DATE_TIME);
+                    m.registerModule(module);
+                });
     }
 }
